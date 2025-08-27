1babd862066b2bd8ab99812bab49a7f06c3123c4
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-08-27 13:11:15 -0300

    Curso React - Fim da Explicação inicial
    Explicação da estrutura básica de um projeto react

--- CursoReact/src/App.jsx
+++ CursoReact/src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+/*
+  App.jsx
+
+  Componente principal do aplicativo React.
+  - Ponto de partida da interface, onde outros componentes podem ser renderizados.
+  - Importa seu próprio arquivo de estilos (App.css) para manter o layout e design específicos do App.
+  - Normalmente contém JSX, lógica, estados (useState) e funções que controlam o comportamento do App.
+  - Pode servir como "pai" de outros componentes, recebendo e passando props conforme necessário.
+  - É exportado como padrão para que o main.jsx possa importá-lo e renderizá-lo no DOM.
+  
+  Observação:
+  - O React usa JSX para criar elementos que serão renderizados dentro do root (div #root do index.html).
+  - Mesmo que o App esteja vazio inicialmente (<> </>), ele é o contêiner principal do aplicativo.
+*/
+
+import './App.css' //importa o app.css
 
 function App() {
- 
-
   return (
     <>
-      
+      {/* Aqui dentro podemos renderizar componentes filhos */}
     </>
   )
 }
 
 export default App
+
